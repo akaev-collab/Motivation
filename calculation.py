2dadb923b6f5d380ede65236f8e0257cc06ec9c8
akaev-collab <69615318+akaev-collab@users.noreply.github.com>
2023-12-12 22:19:03 +0300

    Update calculation.py

--- calculation.py
+++ calculation.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 st.title("Калькулятор")
 st.markdown("### Если за отчетный период был перевод просьба указать, если нет переходи к рассчету премии")
 on = st.toggle("Был перевод")
+
+
+data = pd.read_excel('data.xlsx', sheet_name="P_RD")
+
+st.dataframe(data)
