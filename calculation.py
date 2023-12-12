7910d0d7a2ff04275db30675f8081d38684461a2
akaev-collab <69615318+akaev-collab@users.noreply.github.com>
2023-12-12 22:14:54 +0300

    Update calculation.py

--- calculation.py
+++ calculation.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import json
-import pandas as pd
-import datetime as dt
-from datetime import datetime
-
-from dateutil import relativedelta
 
 st.set_page_config(layout="wide")
 st.title("Калькулятор")
