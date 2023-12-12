8733f840c1a5b71c602ec51a9664bdec671291a4
akaev-collab <69615318+akaev-collab@users.noreply.github.com>
2023-12-13 00:07:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,4 +15,25 @@ def load_data():
 
 df_load_group, df_izm_group, df_izm_group = load_data()
 
+path_to_json = "Structure"
+
+with open (path_to_json +"\structure.json", "r", encoding='utf-8') as file:
+    structure = json.load(file) # зависимость мастерской и группу от управления
+
+d = []
+master_merge_dict = {}
+for i in structure.keys():
+    d.append(structure[i])
+
+for i in d:
+    for key, value in i.items():
+        master_merge_dict[key] = value
+        
+
+selected = option_menu(None, options=["Готовность", "Качество", "Выработка","Инициативы"],
+                            icons=['bar-chart-steps','bricks','hexagon-half','diagram-2'],
+                            orientation="horizontal")
+
+filter_selected_master = st.multiselect("Мастерская", master_merge_dict.keys(), master_merge_dict.keys())
+
 st.dataframe(df_load_group)
