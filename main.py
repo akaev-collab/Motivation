9c0b81ba1d7fa65f73405425b778fe460ed8cdec
akaev-collab <69615318+akaev-collab@users.noreply.github.com>
2023-12-13 00:16:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import json
 
 st.set_page_config(layout="wide")
 st.title("Дашборд")
@@ -17,23 +18,9 @@ df_load_group, df_izm_group, df_izm_group = load_data()
 
 path_to_json = "structure.json"
 
+path_to_json = "Structure_JSON"
+
 with open (path_to_json, "r", encoding='utf-8') as file:
     structure = json.load(file)
-
-d = []
-master_merge_dict = {}
-for i in structure.keys():
-    d.append(structure[i])
-
-for i in d:
-    for key, value in i.items():
-        master_merge_dict[key] = value
-        
-
-selected = option_menu(None, options=["Готовность", "Качество", "Выработка","Инициативы"],
-                            icons=['bar-chart-steps','bricks','hexagon-half','diagram-2'],
-                            orientation="horizontal")
-
-filter_selected_master = st.multiselect("Мастерская", master_merge_dict.keys(), master_merge_dict.keys())
 
 st.dataframe(df_load_group)
