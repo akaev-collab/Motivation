0eee5c31556845ebd18c3ad1dc938cd5d2bb1544
akaev-collab <69615318+akaev-collab@users.noreply.github.com>
2023-12-12 22:13:54 +0300

    Update calculation.py

--- calculation.py
+++ calculation.py
@@ -10,7 +10,3 @@ st.set_page_config(layout="wide")
 st.title("Калькулятор")
 st.markdown("### Если за отчетный период был перевод просьба указать, если нет переходи к рассчету премии")
 on = st.toggle("Был перевод")
-
-df_load = pd.read_excel('data.xlsx', sheet_name="P_RD")
-
-st.dataframe(df_load)
