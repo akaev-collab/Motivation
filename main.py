aaa05f0d30b974167ae983c11700deab1b6068f4
akaev-collab <69615318+akaev-collab@users.noreply.github.com>
2023-12-12 23:38:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.title("New page")
 
 data = pd.DataFrame({"a":[1,24,3], 'b':[2,45,34]})
 
-data_1 = pd.read_csv(data)
+data_1 = pd.read_csv(data, sep = ";")
 
 st.dataframe(data)
 st.dataframe(data_1)
