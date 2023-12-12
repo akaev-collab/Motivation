f91971ba8a09620318831229fb5973c2cba143a9
akaev-collab <69615318+akaev-collab@users.noreply.github.com>
2023-12-12 22:28:16 +0300

    Update calculation.py

--- calculation.py
+++ calculation.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 
 st.set_page_config(layout="wide")
 st.title("Калькулятор")
 st.markdown("### Если за отчетный период был перевод просьба указать, если нет переходи к рассчету премии")
 on = st.toggle("Был перевод")
-
-
