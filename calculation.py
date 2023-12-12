bd85f53d798b828e612e765158051fd0c251e6ef
akaev-collab <69615318+akaev-collab@users.noreply.github.com>
2023-12-12 21:55:36 +0300

    Update calculation.py

--- calculation.py
+++ calculation.py
@@ -10,3 +10,11 @@ st.set_page_config(layout="wide")
 st.title("Калькулятор")
 st.markdown("### Если за отчетный период был перевод просьба указать, если нет переходи к рассчету премии")
 on = st.toggle("Был перевод")
+
+
+uploaded_file = st.file_uploader(
+    "Выбирите данные", accept_multiple_files=False)
+if uploaded_file is not None:
+    file_name = uploaded_file
+else:
+    file_name = "data.xlsx"
