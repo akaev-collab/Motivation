31d7682835b9fb35819ba92f370cb825cb687d8d
akaev-collab <69615318+akaev-collab@users.noreply.github.com>
2023-12-13 00:21:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ def load_data():
     
     file_name = "data.xlsx"
     df_load_group = pd.read_excel(file_name, sheet_name="P_RD_group")
-    df_izm_group = pd.read_excel(file_name, sheet_name="IZM_group")
-    df_productivity_group = pd.read_excel(file_name, sheet_name="Productivity_group")
-    
-    return df_load_group, df_izm_group, df_izm_group
 
-df_load_group, df_izm_group, df_izm_group = load_data()
+    return df_load_group
+
+df_load_group = load_data()
 
 st.dataframe(df_load_group)
