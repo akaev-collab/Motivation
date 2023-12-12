e5e85e1681ac70d785eff5c701d4cc3be3310575
akaev-collab <69615318+akaev-collab@users.noreply.github.com>
2023-12-13 00:25:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,4 +17,19 @@ def load_data():
 
 df_load_group, df_izm_group, df_productivity_group = load_data()
 
+
+path_to_json = "Structure"
+
+with open (path_to_json +"/structure.json", "r", encoding='utf-8') as file:
+    structure = json.load(file)
+
+d = []
+master_merge_dict = {}
+for i in structure.keys():
+    d.append(structure[i])
+
+for i in d:
+    for key, value in i.items():
+        master_merge_dict[key] = value
+
 st.dataframe(df_izm_group)
